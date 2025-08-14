dc0b7327e23e2d868fe635f0b4421b9a148df4bb
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-08-14 15:31:32 -0300

    Criado carousel no Layout

--- SiteVendas/Controllers/HomeController.cs
+++ SiteVendas/Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace SiteVendas.Controllers
         {
             return View();
         }
+        public IActionResult Demo()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
