84e36967daab67ded788c47b8ce35720fafcad9b
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-08-20 14:54:22 -0300

    Criado o Carrinho de Compras

--- SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
+++ SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394117e6676ee176b77533703c06c542edf88d5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b2e93f8475310b8115bb215c2025616c886fe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
