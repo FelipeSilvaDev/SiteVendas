5bc5236c2c379afa21c984420f0139e7dcbfc57c
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-08-22 11:06:09 -0300

    Criado a View Componente para exibir as categorias no menu Lanches

---
+++ SiteVendas/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteVendas.Repositories.Interfaces;
+
+namespace SiteVendas.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categoria = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categoria);
+        }
+    }
+}
--- SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
+++ SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ce973acd055061f9e9461b2609a191a5d58b49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2003e7724bb672145f4682cf667813014446abc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
