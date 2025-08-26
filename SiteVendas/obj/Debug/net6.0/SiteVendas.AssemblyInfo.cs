d0ce2dace926e0e1ff950540c597055c03480624
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-08-26 11:27:56 -0300

    Criado a view Checkout

--- SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
+++ SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97a933e85f856853dd7d87ac38146e0da5e491b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476334a51d6e6d0c3543938706c1b62958f26a9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
