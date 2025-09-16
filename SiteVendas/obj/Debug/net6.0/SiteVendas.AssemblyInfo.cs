69d4f3c1a8e6b3cae494e4aadd714d9698aef926
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-09-16 16:09:12 -0300

    Criado os controller e view para gerenciar Lanches, Categorias e Pedidos da area(Admin)

--- SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
+++ SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0dc90460b2bec64227eb2c57d71fe87b0ebba69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d11eee42ef13e3ae545482781b561a71d367bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
