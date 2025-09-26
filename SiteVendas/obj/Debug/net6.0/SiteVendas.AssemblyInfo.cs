be6cf73b51b457010c056fd267102435da1de779
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-09-26 16:16:14 -0300

    Implementado Exibir e deletar imagens Area(Admin)

--- SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
+++ SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fbd1535b30f45d03574850fb50b7d717a88755d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9123b5b5b59968d0443ebe80033db1f354c081bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
