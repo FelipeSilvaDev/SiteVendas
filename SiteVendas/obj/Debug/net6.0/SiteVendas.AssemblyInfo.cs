2e1caf3c3c1be27e3fa675992fa39cc2f2ccdd74
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-09-30 16:54:00 -0300

    Implementado a configuracao na class Program e removida a classe Startup

--- SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
+++ SiteVendas/obj/Debug/net6.0/SiteVendas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be6cf73b51b457010c056fd267102435da1de779")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a519da5db3db79ebdf553eda11c674842bbf7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteVendas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
